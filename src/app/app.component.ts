6db3bd02395b437c864e7dbaa780d497eb2f2781
rroot47 <boubousylla2@gmail.com>
2022-09-14 00:01:27 +0200

    add component appareil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-appareils';
+isAuth = false;
+appareils = [
+  {
+    name : 'PC',
+    status : 'allumé'
+  },
+  {
+    name : 'Machine à laver',
+    status : 'éteint'
+  },
+  {
+    name : 'Television',
+    status : 'allumé'
+  }
+]
+update = new Date()
+
+updateAsyn = new Promise(
+  (resolve, rejects) =>{
+   const date = new Date();
+   setTimeout(
+    ()=>{
+      resolve(date);
+    }, 2000
+   ); 
+  }
+);
+
+constructor(){
+  setTimeout(
+    ()=>{
+      this.isAuth = true;
+    },2000
+  )
+}
+onAllumer(){
+  console.log("Tout est allumer!!");
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppareilComponent } from './appareil/appareil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppareilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
